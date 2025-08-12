4d4709cc7054f9bac67624aeb2259c323f942022
alcides07 <alcidesdantasdj@gmail.com>
2025-08-12 13:04:47 -0300

    feat: implementa modelo de user.

---
+++ api/src/main/java/com/tarefas/api/model/User.java
@@ -0,0 +1,51 @@
+package com.tarefas.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Table(name="users")
+@Entity(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
