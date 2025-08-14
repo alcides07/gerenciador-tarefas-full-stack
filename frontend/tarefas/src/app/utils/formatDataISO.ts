fe8744a302eef3335bf45b9672f78707df418714
alcides07 <alcidesdantasdj@gmail.com>
2025-08-14 17:51:26 -0300

    feat: implementa formatacao de dataISO.

---
+++ frontend/tarefas/src/app/utils/formatDataISO.ts
@@ -0,0 +1,9 @@
+export default function formatDataISOToddMMyyyy(dataISO: string): string {
+  const data = new Date(dataISO);
+
+  const dia = String(data.getDate()).padStart(2, '0');
+  const mes = String(data.getMonth() + 1).padStart(2, '0');
+  const ano = data.getFullYear();
+
+  return `${dia}/${mes}/${ano}`;
+}
