82af9ac2728cfd9e0c6cd30b5dc6b9d122ba432a
alcides07 <alcidesdantasdj@gmail.com>
2025-08-14 17:53:05 -0300

    feat: implementa servicos de tarefa.

---
+++ frontend/tarefas/src/app/services/tarefa/tarefa.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../src/environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tarefa, TarefaOptionalRequest, TarefaRequest } from '../../interfaces/models/tarefa';
+import { TarefaFilter } from '../../interfaces/filters/tarefa';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TarefaService {
+  private baseApiUrl = environment.baseApiUrl;
+  private tarefaApiUrl = `${this.baseApiUrl}/tarefas/`;
+
+  constructor(private http: HttpClient) {}
+
+  getTarefas(filters: TarefaFilter): Observable<Tarefa[]> {
+    let params = new HttpParams();
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value) {
+        params = params.append(key, value.toString());
+      }
+    });
+
+    return this.http.get<Tarefa[]>(this.tarefaApiUrl, { params });
+  }
+
+  createTarefa(data: TarefaRequest): Observable<Tarefa> {
+    return this.http.post<Tarefa>(this.tarefaApiUrl, data);
+  }
+
+  updateTarefa(data: TarefaOptionalRequest): Observable<Tarefa> {
+    return this.http.patch<Tarefa>(this.tarefaApiUrl, data);
+  }
+
+  completeTarefa(): Observable<Tarefa> {
+    return this.http.patch<Tarefa>(this.tarefaApiUrl, { situacao: 'CONCLUIDA' });
+  }
+
+  deleteTarefa(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.tarefaApiUrl}/${id}`);
+  }
+}
