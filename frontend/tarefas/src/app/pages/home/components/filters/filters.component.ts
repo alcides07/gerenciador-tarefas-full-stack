61621fe0c38ca75a8acca2725ecd79ab9d71b7d0
alcides07 <alcidesdantasdj@gmail.com>
2025-08-14 17:53:43 -0300

    feat: implementa componente de filtros.

---
+++ frontend/tarefas/src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { TarefaFilter } from '../../../../interfaces/filters/tarefa';
+import { Prioridade, Situacao } from '../../../../interfaces/models/tarefa';
+import { FormsModule } from '@angular/forms';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { Responsavel } from '../../../../interfaces/models/responsavel';
+import {
+  NzFormControlComponent,
+  NzFormItemComponent,
+  NzFormLabelComponent,
+} from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+@Component({
+  selector: 'app-filters',
+  imports: [
+    FormsModule,
+    NzSelectModule,
+    NzButtonModule,
+    NzInputModule,
+    NzFormItemComponent,
+    NzFormLabelComponent,
+    NzFormControlComponent,
+  ],
+  templateUrl: './filters.component.html',
+  styleUrl: './filters.component.css',
+})
+export class FiltersComponent {
+  @Input() responsaveis: Responsavel[] = [];
+  @Output() aplicarFiltros = new EventEmitter<TarefaFilter>();
+
+  filtros: Partial<TarefaFilter> = {};
+  prioridades: Prioridade[] = ['BAIXA', 'MEDIA', 'ALTA'];
+  situacoes: Situacao[] = ['EM_ANDAMENTO', 'CONCLUIDA'];
+
+  aplicar() {
+    const validFiltros: TarefaFilter = {
+      tarefaId: this.filtros.tarefaId || undefined,
+      search: this.filtros.search || undefined,
+      responsavelId: this.filtros.responsavelId || undefined,
+      prioridade: this.filtros.prioridade || undefined,
+      situacao: this.filtros.situacao || undefined,
+    };
+    this.aplicarFiltros.emit(validFiltros);
+  }
+}
