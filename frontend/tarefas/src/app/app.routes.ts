9d5548a495c86f2341c75c7ecdfee792976ac70b
alcides07 <alcidesdantasdj@gmail.com>
2025-08-14 17:46:53 -0300

    feat: configura rotas.

--- frontend/tarefas/src/app/app.routes.ts
+++ frontend/tarefas/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent,
+  },
+  {
+    path: 'inicio',
+    component: HomeComponent,
+  },
+];
