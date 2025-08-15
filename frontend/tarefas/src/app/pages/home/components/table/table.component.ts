a2c93e2edbf771bbbb9a527bb5ad4da91ad7cd1b
alcides07 <alcidesdantasdj@gmail.com>
2025-08-15 13:07:10 -0300

    refactor: remove logicas antigas em table.

--- frontend/tarefas/src/app/pages/home/components/table/table.component.ts
+++ frontend/tarefas/src/app/pages/home/components/table/table.component.ts
@@ -1,14 +1,13 @@
-import { ChangeDetectorRef, Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { Tarefa } from '../../../../interfaces/models/tarefa';
 import formatDataISOToddMMyyyy from '../../../../utils/formatDataISO';
 import { NzModalService } from 'ng-zorro-antd/modal';
 import { TarefaService } from '../../../../services/tarefa/tarefa.service';
-import { finalize } from 'rxjs';
-import { NzMessageService } from 'ng-zorro-antd/message';
 import { ModalDeleteTarefaComponent } from '../modal-delete-tarefa/modal-delete-tarefa.component';
 import { ModalCompleteTarefaComponent } from '../modal-complete-tarefa/modal-complete-tarefa.component';
+import { ModalCreateTarefaComponent } from '../modal-create-tarefa/modal-create-and-update-tarefa.component';
 
 @Component({
   selector: 'app-table',
@@ -17,68 +16,37 @@ import { ModalCompleteTarefaComponent } from '../modal-complete-tarefa/modal-com
     NzTableModule,
     ModalDeleteTarefaComponent,
     ModalCompleteTarefaComponent,
+    ModalCreateTarefaComponent,
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
 })
 export class TableComponent {
   @Input() tarefas: Tarefa[] = [];
+
   @Output() onDeleteTarefaEmit = new EventEmitter<void>();
+  @Output() onUpdateTarefaEmit = new EventEmitter<void>();
   @Output() onCompleteTarefaEmit = new EventEmitter<void>();
 
-  isLoadingSubmit = false;
-  modal: NzModalService = inject(NzModalService);
-  tarefaService: TarefaService = inject(TarefaService);
-  private cdr = inject(ChangeDetectorRef);
-  private message = inject(NzMessageService);
   selectedTarefa: Tarefa | null = null;
   showDeleteModal = false;
+  showUpdateModal = false;
   showCompleteModal = false;
 
   formatarData(dataISO: string): string {
     return formatDataISOToddMMyyyy(dataISO);
   }
 
-  handleDeleteTarefa(id: number): void {
-    this.isLoadingSubmit = true;
-    this.tarefaService
-      .deleteTarefa(id)
-      .pipe(
-        finalize(() => {
-          this.isLoadingSubmit = false;
-          this.cdr.markForCheck();
-          this.showDeleteModal = false;
-        })
-      )
-      .subscribe({
-        next: () => {
-          this.onDeleteTarefaEmit.emit();
-        },
-        error: () => {
-          this.message.create('error', 'Erro ao excluir tarefa!');
-        },
-      });
+  onModalCompleteConfirm(): void {
+    this.onCompleteTarefaEmit.emit();
   }
 
-  handleCompleteTarefa(id: number): void {
-    this.isLoadingSubmit = true;
-    this.tarefaService
-      .completeTarefa(id)
-      .pipe(
-        finalize(() => {
-          this.isLoadingSubmit = false;
-          this.cdr.markForCheck();
-          this.showCompleteModal = false;
-        })
-      )
-      .subscribe({
-        next: () => {
-          this.onCompleteTarefaEmit.emit();
-        },
-        error: () => {
-          this.message.create('error', 'Erro ao concluir tarefa!');
-        },
-      });
+  onModalDeleteConfirm(): void {
+    this.onDeleteTarefaEmit.emit();
+  }
+
+  onModalUpdateConfirm(): void {
+    this.onUpdateTarefaEmit.emit();
   }
 
   openDeleteModal(tarefa: Tarefa): void {
@@ -86,22 +54,19 @@ export class TableComponent {
     this.showDeleteModal = true;
   }
 
+  openUpdateModal(tarefa: Tarefa): void {
+    this.selectedTarefa = tarefa;
+    this.showUpdateModal = true;
+  }
+
   openCompleteModal(tarefa: Tarefa): void {
     this.selectedTarefa = tarefa;
     this.showCompleteModal = true;
   }
 
-  onModalDeleteConfirm(id: number): void {
-    this.handleDeleteTarefa(id);
-  }
-
-  onModalCompleteConfirm(id: number): void {
-    this.handleCompleteTarefa(id);
-  }
-
   onModalCancel(): void {
     this.showDeleteModal = false;
     this.showCompleteModal = false;
-    this.selectedTarefa = null;
+    this.showUpdateModal = false;
   }
 }
